cd963b35a51c7ce5d26776d58c664542347cc83a
sadiq_from_ubuntu_server <sadiqonemail@gmail.com>
2024-08-15 16:44:02 +0000

    cors installed and config added

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
+import { corsOptions } from '#cors';
 import { rootRouter } from '#routes';
 
 const app = express();
+
+// Middlewares
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app.get('/', rootRouter);
 
---
+++ cors/index.js
@@ -0,0 +1,2 @@
+export { default as allowedOrigins } from './allowedOrigins.js';
+export { default as corsOptions } from './corsOptions.js';
