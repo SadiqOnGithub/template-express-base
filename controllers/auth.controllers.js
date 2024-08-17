785e7b9528d785d2bdbc14a7de19155ca6cdf8d9
sadiq_from_ubuntu_server <sadiqonemail@gmail.com>
2024-08-17 21:56:46 +0000

    refersh controller added with central error hanlding

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,7 +1,29 @@
+import { AppError } from '#errors'
+import { Admin } from '#models'
+import jwt from 'jsonwebtoken'
+
 const adminLogin = (req, res) => {
   res.send('api is working !!!!!!')
 }
 
+const refresh = async (req, res) => {
+  const cookies = req.cookies
+
+  if (!cookies?.jwt) throw new AppError('No refresh token found', 401)
+
+  const refreshToken = cookies.jwt
+
+  const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
+  const foundUser = await Admin.findById(decoded._id)
+
+  if (!foundUser) throw new AppError('User not found', 401)
+
+  const accessToken = foundUser.generateAccessToken()
+
+  res.json({ accessToken })
+}
+
 export default {
   adminLogin,
+  refresh,
 }
