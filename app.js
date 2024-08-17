2dd49e475e0de67b54e35c3995004d94b561b694
sadiq_from_ubuntu_server <sadiqonemail@gmail.com>
2024-08-17 22:29:23 +0000

    cookie-parser added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 import { corsOptions } from '#cors'
 import { authRouter, rootRouter } from '#routes'
@@ -9,6 +10,7 @@ const app = express()
 // Middlewares
 app.use(cors(corsOptions))
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/', rootRouter)
 app.use('/auth', authRouter)
