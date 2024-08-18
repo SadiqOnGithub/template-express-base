104c157ab5bacfab27a6087d816c0a919eda344c
sadiq_from_ubuntu_server <sadiqonemail@gmail.com>
2024-08-18 23:36:56 +0000

    correction in authMiddle

--- middlewares/authMiddleware.middlewares.js
+++ middlewares/authMiddleware.middlewares.js
@@ -5,15 +5,15 @@ const authMiddleware = (req, res, next) => {
   // 1. Get the token from the Authorization header
   const authHeader = req.headers.authorization || req.headers.Authorization
   if (!authHeader?.startsWith('Bearer ')) {
-    return new AppError('No token provided', 401)
+    throw new AppError('No token provided', 401)
   }
   const token = authHeader.split(' ')[1]
-
   // 2. Verify the token
   const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
 
-  // 3. Attach the username to the request object
-  req.admin = decoded.username
+  // 3. Attach the username and _id to the request object
+  req.id = decoded._id
+  req.username = decoded.username
 
   next()
 }
