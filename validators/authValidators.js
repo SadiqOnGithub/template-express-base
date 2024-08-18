bbd2adb1d5ff7f9f8fabb9ebc106543468caa064
sadiq_from_ubuntu_server <sadiqonemail@gmail.com>
2024-08-18 11:01:31 +0000

    validateLoginInput and loginSchema added

---
+++ validators/authValidators.js
@@ -0,0 +1,27 @@
+import Joi from 'joi'
+
+// eslint-disable-next-line no-unused-vars
+const loginSchema = Joi.object({
+  username: Joi.string().min(3).max(30).required().messages({
+    'string.empty': 'Username is required',
+    'string.min': 'Username must be at least 3 characters long',
+    'string.max': 'Username cannot exceed 30 characters',
+  }),
+  password: Joi.string().min(8).required().messages({
+    'string.empty': 'Password is required',
+    'string.min': 'Password must be at least 8 characters long',
+  }),
+})
+
+const loginSchemaTesting = Joi.object({
+  username: Joi.string().required().messages({
+    'string.empty': 'Username is required',
+  }),
+  password: Joi.string().required().messages({
+    'string.empty': 'Password is required',
+  }),
+})
+
+export const validateLoginInput = (data) => {
+  return loginSchemaTesting.validate(data)
+}
---
+++ validators/index.js
@@ -0,0 +1,1 @@
+export { validateLoginInput } from './authValidators.js'
