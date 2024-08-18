11ebd037a0d4e66b9e28092d31cbe70e30ab1ce0
sadiq_from_ubuntu_server <sadiqonemail@gmail.com>
2024-08-18 13:38:23 +0000

    admin model updated, presave added for password hashing

--- models/Admin.models.js
+++ models/Admin.models.js
@@ -3,22 +3,52 @@ import { model, Schema } from 'mongoose'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 
-const adminSchema = Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
+const adminSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, 'Please provide a username'],
+      unique: true,
+      trim: true,
+      // minlength: 3,
+      // maxlength: 30,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide a password'],
+      // minlength: 8,
+      select: false,
+    },
+    refreshToken: {
+      type: String,
+      select: false,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
+  { timestamps: true },
+)
+
+// Pre-save hook to hash password
+adminSchema.pre('save', async function (next) {
+  // Only hash the password if it has been modified (or is new)
+  if (!this.isModified('password')) return next()
+
+  try {
+    // Generate a salt
+    const salt = await bcrypt.genSalt(10)
+    // Hash the password along with our new salt
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+  } catch (error) {
+    next(error)
+  }
 })
 
+// Method to check if password is correct
 adminSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password)
 }
 
+// Method to generate access token
 adminSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     { _id: this._id, username: this.username },
@@ -27,6 +57,7 @@ adminSchema.methods.generateAccessToken = function () {
   )
 }
 
+// Method to generate refresh token
 adminSchema.methods.generateRefreshToken = function () {
   return jwt.sign(
     { _id: this._id },
